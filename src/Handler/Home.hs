d26bc4ec9c89f47e1a17d47a8f61346711468623
Raphael Javaux <raphaeljavaux@gmail.com>
2012-11-04 22:24:50 +0100

    upload ressource

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -3,13 +3,13 @@ module Handler.Home where
 
 import Import
 
-import Handler.Upload (Options, optionsForm)
+import Handler.Upload (optionsForm)
 
 -- Shows the home page
 getHomeR :: Handler RepHtml
 getHomeR = do
-    (widget, enctype) <- generateFormPost $ renderDivs $ aopt emailField "Send link by email" Nothing
-    
+    (widget, enctype) <- generateFormPost optionsForm
+
     defaultLayout $ do
         setTitle "getwebb - Free file sharing"
         addScriptRemote "http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"
--- src/Handler/Upload.hs
+++ src/Handler/Upload.hs
@@ -9,9 +9,22 @@ data Options = Options {
       optEmail :: Maybe Text
     }
 
+-- | Creates a form for the upload options.
 optionsForm :: Html -> MForm App App (FormResult Options, Widget)
-optionsForm =
-    renderDivs $ Options <$> aopt emailField "Send link by email" Nothing
+optionsForm extra = do
+    let settings = FieldSettings {
+          fsLabel = "Send link by email", fsTooltip = Nothing
+        , fsId = Just "options_email", fsName = Just "options_email"
+        , fsAttrs = []
+    }
+    (emailRes, emailView) <- mopt emailField settings Nothing
+    let widget = do
+        [whamlet|
+<label for=^{toHtml $ fvId emailView}>^{fvLabel emailView}:
+^{fvInput emailView}
+^{extra}
+|]
+    return (Options <$> emailRes, widget)
 
 -- Uploads files to the server. Returns a Json object which contains the
 -- id of the upload or the error.

1a67d0388691b1a3369f1babf7bf3930a5fb6c8c
Raphael Javaux <raphaeljavaux@gmail.com>
2012-11-23 22:54:31 +0100

    ffmpeg command

--- src/Upload/FFmpeg.hs
+++ src/Upload/FFmpeg.hs
@@ -7,7 +7,11 @@ import System.Process (runInteractiveProcess)
 executable :: FilePath
 executable = "/usr/bin/ffmpeg"
 
-runInteractiveProcess executable 
+time ffmpeg -i sintel_trailer-1080p.mp4 -s hd720 -vpre libvpx-720p -deadline realtime -f webm -ab 112k -y test.webm
 
-ffmpeg -i pipe:0
-ffmpeg -i pipe:1
+time ffmpeg -i sintel_trailer-1080p.mp4 -s hd720 -vcodec libx264 -preset veryfast -acodec libmp3lame -ab 112k -y test.mkv
+
+
+ffmpeg -i /home/rapha/sintel_trailer-1080p.mp4 -s 1280x720 -vpre libvpx-720p -f webm -ab 128k -y test.webm
+["-s", "hd720", "-vpre", "libvpx-720p", "-f", "webm", "-ab", "128k", "-y", "-i", "pipe:0" "pipe:1"]
+runInteractiveProcess executable 
